be43ab33cf1a8e010443601ab6ea9c125d50fea3
Jonas-au18 <jonas.sdu@gmail.com>
2020-03-03 09:17:40 +0100

    UDP client - needs cleanup

---
+++ UDP/UDP-Client/program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+
+namespace UDP_Client
+{
+
+	class UDP_Client
+	{
+		string ipAddress = "10.0.0.1";
+        
+		public UDP_Client(string[] args)
+		{
+        try
+			{
+				using (var client = new UdpClient())
+				{
+					IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ipAddress), 9000);
+					client.Connect(endPoint);
+					byte[] handshake = Encoding.ASCII.GetBytes("a");
+					client.Send(handshake, handshake.Length);
+					Console.WriteLine(args[0]);
+					byte[] toSend = Encoding.ASCII.GetBytes(args[0]);
+					client.Send(toSend, toSend.Length);
+                    byte[] recv = new byte[1024];
+
+                 
+						recv = client.Receive(ref endPoint);
+						Console.WriteLine(Encoding.ASCII.GetString(recv));
+					
+					Console.WriteLine("Closing");
+					client.Close();
+				}
+
+			}
+            catch(Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+ 
+ }
+
+
+		class MainClass
+		{
+			public static void Main(string[] args)
+			{
+				Console.WriteLine(args[0]);
+
+				new UDP_Client(args);
+			}
+		}
+	}
+}
---
+++ UDP/UDP-Server/UDP-Server/server.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+
+
+
+namespace UDPServer
+{
+	class UDP_Server
+	{
+    
+		int listenPort = 9000;
+		public UDP_Server()
+		{
+			int recv;
+			byte[] data = new byte[1024];
+			IPEndPoint ipep = new IPEndPoint(IPAddress.Any, listenPort);
+
+			Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			mySocket.Bind(ipep);
+			Console.WriteLine("Waiting on a client...");
+			IPEndPoint sender = new IPEndPoint(IPAddress.Any, 9000);
+			EndPoint remote = (EndPoint)(sender);
+
+			while (true)
+			{
+				data = new byte[1024];
+				recv = mySocket.ReceiveFrom(data, ref remote);
+
+
+
+				recv = mySocket.ReceiveFrom(data, ref remote);
+				string recieved = Encoding.ASCII.GetString(data);
+                
+				if (string.Compare(recieved,"u") == 0 || string.Compare(recieved,"U") == 0){
+					
+                    byte[] toSend = new byte[1024];
+					FileStream fs = new FileStream("/proc/uptime", FileMode.Open, FileAccess.Read);
+                    int read = fs.Read(toSend, 0, toSend.Length);
+					mySocket.SendTo(toSend, toSend.Length, SocketFlags.None, remote);
+
+                    fs.Flush();
+					fs.Close();
+			}
+
+
+
+				else if(string.Compare(recieved, "l") == 0 || string.Compare(recieved, "L") == 0)			
+						{
+
+							byte[] toSend = new byte[1024];
+                             FileStream fs = new FileStream("/proc/loadavg", FileMode.Open, FileAccess.Read);
+                            int read = fs.Read(toSend, 0, toSend.Length);
+
+					mySocket.SendTo(toSend, toSend.Length, SocketFlags.None, remote);
+	
+					fs.Flush();
+					fs.Close();
+				
+						}
+				else{
+
+				}
+				}
+                
+        
+				
+	}
+
+}
+
+
+
+       class MainClass
+		{
+			public static void Main(string[] args)
+			{
+				Console.WriteLine("I haz become sentient");
+				new UDP_Server();
+			}
+		}
+	}
+
+    
